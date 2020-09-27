1afa9b8afa3839215ae7a48afb7a23f0c94fdca6
Mariana Freitas <69424170+marianafreitas13@users.noreply.github.com>
2020-09-26 21:35:09 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,8 @@
+const  app  =  require  ( './src/app' ) ;
+ porta  const =  8080 ;
+
+app . escute  ( porta ,  ( )  =>  {
+
+    console . log  ( `App esta rodando $ { port } ` )
+} ) ;
+© 2020 GitHub, Inc.
